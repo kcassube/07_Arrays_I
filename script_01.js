373b6cf06a97ee3294f0cc745a46653aedd5f6a7
kcassube <79251426+kcassube@users.noreply.github.com>
2023-07-19 10:08:11 +0200

    Class

--- script_01.js
+++ script_01.js
@@ -8,9 +8,16 @@
  */
 
 /*** 02. Theorie: Array */
+let arr;
+// arr = new Array();
+arr = []; // Litteral
+arr = [2,7,11,4,8,10];
 
-
-
+output(arr);
+output(arr.length);
+output(arr[0]); // index 0 (1 Position im Array)
+output(arr[4]);
+output(arr[arr.length-1]); // letzte Pos. / allgemein
 
 /**
  *   PRAXIS
@@ -21,14 +28,16 @@
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
-
-function getSentence(word1,word2,word3,word4) {
+output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
+function getSentence(word1,word2,word3,word4,word5,word6) {
     const GAP = " ";
     const PUNCT = ".";
     let str =   word1 + GAP +
                 word2 + GAP +
                 word3 + GAP +
                 word4 + GAP +
+                word5 + GAP +
+                word6 +
                 PUNCT;
 
     return str;
