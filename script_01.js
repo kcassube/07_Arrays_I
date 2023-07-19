f7f76e6f450d3c32b733975723a18f80a698327f
kcassube <79251426+kcassube@users.noreply.github.com>
2023-07-19 10:43:35 +0200

    class_tech02

--- script_01.js
+++ script_01.js
@@ -8,16 +8,37 @@
  */
 
 /*** 02. Theorie: Array */
-let arr;
-// arr = new Array();
-arr = []; // Litteral
-arr = [2,7,11,4,8,10];
+// let arr;
+// // arr = new Array(); // Konstruktor
+// arr = []; // Literal
+// arr = [2,7,11,4,8,10];
 
-output(arr);
-output(arr.length);
-output(arr[0]); // index 0 (1 Position im Array)
-output(arr[4]);
-output(arr[arr.length-1]); // letzte Pos. / allgemein
+// output(arr);
+// output(arr.length); // Anzahl der Elemente
+// output(arr[0]);  // Index 0 (1. Pos. im Array)
+// output(arr[4]); // letze Pos. / konkret
+// output(arr[arr.length-1]); // letze Pos. / allgemein
+
+/* 02a. Theorie: Schleifen (for-schleife) */
+
+/* For -Schleifen als allg. Wiederholungs-Struktur */
+
+// Inkrement (untere Grenze --> obere Grenze)
+// for (let i = 0; i < 10; i++) {
+//     output(i);   
+// }
+
+// Dekrement (obere Grenze --> untere Grenze)
+// for (let i = 10; i > 0; i--) {
+//     output(i);   
+// }
+
+// Inkrement (var. Schrittweite)
+for (let i = 50; i <= 100; i+= 2) {
+    output(i);   
+}
+
+
 
 /**
  *   PRAXIS
@@ -28,7 +49,7 @@ output(arr[arr.length-1]); // letzte Pos. / allgemein
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
-output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
+// output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
 function getSentence(word1,word2,word3,word4,word5,word6) {
     const GAP = " ";
     const PUNCT = ".";
@@ -43,6 +64,38 @@ function getSentence(word1,word2,word3,word4,word5,word6) {
     return str;
 }
 
+/*** 01a. Funktionalität mit Array 1 */
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
+// output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   arr[0] + GAP +
+                arr[1] + GAP +
+                arr[2] + GAP +
+                arr[3] + GAP +
+                arr[4] + GAP +
+                arr[5] + 
+                PUNCT;
+
+    return str;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -51,5 +104,3 @@ function getSentence(word1,word2,word3,word4,word5,word6) {
 function output(outputStr) {
     console.log(outputStr);
 }
-
-
